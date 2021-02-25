a26e0d30267e8b868f4868a57ebf186403ca44d9
Stephan <github@athome.zone>
2021-02-25 22:24:04 +0100

    Initial HOME

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HOME
- 
+
+I will add a better Readme - Promissed ;-)
+
+The Image for the SD Card is from this place: https://github.com/hypriot/image-builder-rpi/releases
+Also I had very much help from https://www.smarthomebeginner.com!
---
+++ copyCloud.ps1
@@ -0,0 +1,11 @@
+Write-Output "------- Start Deleting -------"
+Remove-Item -Path J:\config.txt -Verbose
+Remove-Item -Path J:\meta-data -Verbose
+Remove-Item -Path J:\network-config -Verbose
+Remove-Item -Path J:\user-data -Verbose
+Write-Output "------- Done Deleting -------"
+Write-Output "------- Start Moving -------"
+Copy-Item -Path .\HOMEcloud\* -Destination J:\ -Recurse -Verbose
+Copy-Item -Path .\secrets -Destination J:\containers\ -Recurse -Verbose
+Copy-Item -Path .\config.txt -Destination J:\ -Recurse -Verbose
+Write-Output "------- Done Moving -------"
